982c51158050e067d8b4af673b260fa1d5bec0c5
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2026-08-28 14:34:09 -0300

    feat(database): add postgres connection handler

--- cmd/schedule/main.go
+++ cmd/schedule/main.go
@@ -5,6 +5,7 @@ import (
 
 	_ "ariga.io/atlas-provider-gorm/gormschema"
 	"github.com/egomes/schedule/internal/env"
+	"github.com/egomes/schedule/internal/repositories"
 	"github.com/egomes/schedule/internal/routes"
 	"github.com/egomes/schedule/internal/server"
 	_ "github.com/joho/godotenv/autoload"
@@ -15,6 +16,10 @@ func main() {
 
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	if err := repositories.DatabaseConnection(verifiedEnvs.DatabaseUrl); err != nil {
+		panic(err)
 	}
 
 	mux := routes.New()
